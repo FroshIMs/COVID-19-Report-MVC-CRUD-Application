58949a7d02576e198627fe9e1aa8dd4fd1a7dd9f
froshims <lazarusgrey37@gmail.com>
2020-03-16 22:42:25 -0700

    COVID-19 Report MVC CRUD Application

--- Views/EditCase.php
+++ Views/EditCase.php
@@ -10,8 +10,8 @@
   <a href='javascript:self.history.back();' class="uk-button uk-button-secondary uk-align-right">Never mind</a>
   <h1 class="uk-heading">COVID-19 Case Edit</h1>
   <form class="uk-form-horizontal uk-margin-large" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <input type="hidden" name="id" value="<?php echo $case['id']; ?>">
     <fieldset class="">
+    <input type="hidden" name="id" value="<?php echo $case['id']; ?>">
         <div class="uk-margin">
           <label class="uk-form-label" for="form-horizontal-text">Case Title</label>
             <div class="uk-form-controls">
@@ -59,7 +59,7 @@
         <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid uk-align-right">
             <label><input class="uk-checkbox" type="checkbox" required> I agree to Terms?</label>
         </div>
-    </fieldset>
     <input type="submit" name="update" value="Update" class="uk-button uk-button-primary uk-align-right">
+    <fieldset>
   </form>
 </div>
